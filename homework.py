62bb9ff4831655097c186aa61846d1eaef086b1e
Sophiya-ai <sophiyabr@gmail.com>
2024-12-17 13:58:15 +0200

    cert added

--- homework.py
+++ homework.py
@@ -1,20 +1,17 @@
 from flask import Flask, render_template
 import requests
-from bootstrap_flask import Bootstrap
-# другой вариант подключения Bootstrap. на официальный сайт Bootstrap: [https://getbootstrap.com](https://getbootstrap.com).
-#на главной странице выбераем версию Bootstrap, которую  хотим использовать
-# На странице загрузки в секции "Compiled CSS and JS" кнопка для загрузки "Download".
-# Качаем скомпилированные файлы Bootstrap, включая `bootstrap.min.css`
+import certifi
+#библиотеку certifi чтобы гарантировать, что Python использует доверенные корневые сертификаты
+# Установить библиотеку, если она у вас не установлена
 
 app = Flask(__name__)
-Bootstrap(app) # подключаем Bootstrap
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-#methods=['GET'] по умолчанию
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def randomQuote():
     try:
-        response = requests.get('https://api.quotable.io/random')
+        response = requests.get('https://api.quotable.io/random', verify=certifi.where())
         response.raise_for_status()  # Проверка на ошибки HTTP
         data = response.json() #Извлекаем данные из JSON-ответа
         quote = data.get('content', 'Не удалось получить цитату')
